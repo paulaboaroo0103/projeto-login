4ecd332e2ffb2903664f509453306b73b568bad6
Paula Vitória <paulavitoriaboaro.12345@gmail.com>
2023-06-20 07:34:46 -0300

    tela de login em andamento

---
+++ logi.js
@@ -0,0 +1,19 @@
+
+
+function Verificar(){
+
+ 
+
+    let email = document.getElementById('loginEmail').value;
+    
+    let senha = document.getElementById('loginSenha').value;
+    
+     
+    
+    if(!email || !senha){
+    
+        alert("Campos de preenchimento obrigatório. Favor preencher");
+    
+    }
+    
+    }
